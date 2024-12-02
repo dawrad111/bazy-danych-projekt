b5f8a77c1d78e069f18beff877ecb815e56622c1
DrHakimmm <272203@student.pwr.edu.pl>
2024-12-02 19:02:58 +0100

    Update phonenumber.sql

--- Users/phonenumber.sql
+++ Users/phonenumber.sql
@@ -2,7 +2,7 @@ CREATE TABLE PhoneNumber (
     id SERIAL PRIMARY KEY, 
     userId SERIAL REFERENCES Users(userId) NOT NULL, 
     phoneNumber TEXT NOT NULL, 
-    link VARCHAR,
+    code VARCHAR,
     linkExpiredDate TIMESTAMP,
     isVerified BOOLEAN DEFAULT FALSE NOT NULL, 
     verifiedDate TIMESTAMP 
